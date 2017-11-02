e35a397f4e528471f8570d67e7cfe837cad14f06
Caroline Forsyth <uncforsyth@gmail.com>
2017-11-02 15:37:38 -0400

    css overhaul

--- assets/javascript/schedule.js
+++ assets/javascript/schedule.js
@@ -32,7 +32,7 @@ $(document).ready(function () {
 
         database.ref().push(addTrain);
 
-        alert("Train successfully added");
+        alert("Train " + trainName + " was successfully added!");
 
         $("#nameInput").val("");
         $("#destInput").val("");
@@ -56,7 +56,7 @@ $(document).ready(function () {
         //Push time back one year
         var dConverted = moment(childSnapshot.val().departsAt, "HH:mm").subtract(1, "years");
 
-        var trainTime = moment(dConverted).format("HH:mm");    
+        var trainTime = moment(dConverted).format("HH:mm");
 
         //difference between times
         var timeConverted = moment(trainTime, "HH:mm").subtract(1, "years");
@@ -72,27 +72,18 @@ $(document).ready(function () {
         //next train
         var nextTrain = moment().add(minsAway, "minutes").format("hh:mm A");
 
-        //create edit and delete button
-        var editBtn = $("<button id="editBtn" class="btn btn-primary" type="submit">Edit</button>");
-
-        var deleteBtn = $("<button id="deleteBtn" class="btn btn-primary" type="submit">Delete</button>").click(function () {
-                            $(this).parent("tr:first").remove(); 
-                        });
-        });
-
-        $("#train-schedule > tbody").append("<tr><td>" + trainName + "</td><td>" 
-        + destination + "</td><td>" + frequency + "min" + "</td><td>" + nextTrain 
-        + "</td><td>" + minsAway + "</td><td>" + editBtn + "</td><td>" + deleteBtn );
+        $("#train-schedule > tbody").append("<tr><td>" + trainName + "</td><td>" +
+            destination + "</td><td>" + frequency + " min" + "</td><td>" + nextTrain +
+            "</td><td>" + minsAway + "</td></tr>");
     });
-
-    var restrict = $(input).attr("restrict");
-    for (var i = 0; i < restrict.length; i++) {
-        restrict[i].addEventListener("keyup", function (e) {
-            var reg = /[0-9|\b|/]/;
-            //Add colon if string length > 2 and string is a number
-            if (this.value.length == 2 && reg.test(this.value)) this.value = this.value + ":"; 
-            //Delete the last digit if string length > 5
-            if (this.value.length > 5) this.value = this.value.substr(0, this.value.length - 1);
-        });
-    };
 });
+
+//Automatically insert colon after hours typed
+var timeInput = $("#firstTimeInput");
+for (var i = 0; i < timeInput.length; i++) {
+    timeInput[i].addEventListener('keyup', function (e) {
+        var reg = /[0-9]/;
+        //Add colon if string length > 2 and string is a number
+        if (this.value.length == 2 && reg.test(this.value)) this.value = this.value + ":";
+    });
+};
